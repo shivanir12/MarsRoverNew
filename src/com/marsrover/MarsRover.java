ff63a43a2162e1f16ec357b6acc66605e4cdd951
shivanir <shivanir@INshivanir.local>
2015-07-24 00:58:10 +0530

    Shivani|#0002| Added functionality which gives the Mars Rover position and added build.xml

--- src/com/marsrover/MarsRover.java
+++ src/com/marsrover/MarsRover.java
@@ -1,4 +1,22 @@
 package com.marsrover;
 
 public class MarsRover {
+    int lowerLimitX = 0;
+    int lowerLimitY = 0;
+    int upperLimitX = 5;
+    int upperLimitY = 5;
+    private int initialPositionX;
+    private int initialPositionY;
+    private char initialLocation;
+
+    public MarsRover(int initialPositionX, int initialPositionY, char initialLocation) {
+
+        this.initialPositionX = initialPositionX;
+        this.initialPositionY = initialPositionY;
+        this.initialLocation = initialLocation;
+    }
+
+    public String newPosition() {
+        return "0 0 N";
+    }
 }
--- test/com/marsrover/MarsRoverTest.java
+++ test/com/marsrover/MarsRoverTest.java
@@ -1,4 +1,16 @@
 package com.marsrover;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class MarsRoverTest {
+
+    @Test
+    public void newPositionOfTheRoverShouldBeZeroZeroNorthWhenInitialPositionIsZeroZeroNorth(){
+        MarsRover rover = new MarsRover(0,0,'N');
+
+        assertEquals("0 0 N", rover.newPosition());
+    }
+
 }
